2826042cc6816078005a9794a6d46a97ab9a3362
Dan Smestad <hockeydan25@gmail.com>
2021-11-28 12:07:59 -0600

    update App message

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')// express simular to python import
 let router = express.Router()// makes a object that understands to match requests to paths to respond to requests. 
 // (/)-the mapping, get starts.req is request, res is responds and next not used here.  
 router.get('/', function(req, res, next) { 
-    res.json({'message': 'Okay whats next!'})
+    res.json({'message': 'Okay, that was fun, what is next!'})
 })
 module.exports = router
 // module.exports pacakges for another file with be able to request and access the router file path. = router
